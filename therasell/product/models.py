21f3ef696e14dddca6650ef873832928be37ee8e
SonakshiRaizada <sr4691@nyu.edu>
2019-11-02 12:39:02 -0400

    Added Procuct class in models

---
+++ therasell/product/models.py
@@ -0,0 +1,22 @@
+
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+# Create your models here.
+class Product(models.Model):
+
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    product_id = models.IntegerField(primary_key = True)
+    product_description = models.TextField()
+    title = models.CharField(max_length=200)
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
