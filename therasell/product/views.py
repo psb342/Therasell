9f0fe45e9c7751a735b71ac2aa3ef15387efc778
SonakshiRaizada <sr4691@nyu.edu>
2019-11-08 01:11:08 -0500

    bootstrap preliminary

--- therasell/product/views.py
+++ therasell/product/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Product
 
 # Create your views here.
 
 def product_list(request):
-    return render(request, 'product/product_list.html', {})
+    products = Product.objects.filter(product_id=1)
+    return render(request, 'product/product_list.html', {'products': products})
