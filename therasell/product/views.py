8158f71aa0002e2d48013f275d4d87677a76f7fe
SonakshiRaizada <sr4691@nyu.edu>
2019-12-08 20:41:47 -0500

    Added dropdown for quantity

--- therasell/product/views.py
+++ therasell/product/views.py
@@ -51,6 +51,12 @@ def product_list(request):
 
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
+    quant = product.Quantity
+    arr = []
+    for i in range(quant):
+        arr.append(i+1)
+
+    product.Quantity = arr
     return render(request, 'product/product_detail.html', {'product': product})
 
 def list_new(request):
